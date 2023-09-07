d546c7ea4eab5d1778bef8f1ef66a66c1d4ef707
Dong-yeob <rkato0128@naver.com>
2023-09-07 20:37:32 +0900

    프로젝트 내용 추가

--- src/constant/project.js
+++ src/constant/project.js
@@ -1,5 +1,321 @@
 export const PROJECT_CONSTANT = [
   {
+    name: "Project MechaDoge",
+    genre: ["모바일", "Unity", "액션", "캐주얼"],
+    date: "21.06. - 23.",
+    listDescription: "",
+    description:
+      "최강의 메카 도지를 키워보자!<br/>귀여운 캐릭터와 강화할 수 있는 다양한 장비들을 다채롭게 조합하여 성장하는 재미의 모바일 캐주얼 핵앤슬래시 MMORPG입니다.<br/>22년 말에 필리핀과 미국 마켓에 소프트 런칭을 완료했습니다.",
+    iconUrl: [
+      "/images/icon/Icon_Tool_Unity.png",
+      "/images/icon/Icon_Tool_Ps.png",
+      "/images/icon/Icon_Tool_Ai.png",
+    ],
+    imageUrl: [
+      "/images/project/Doge/0_Tiltle.webp",
+      "/images/project/Doge/2_Play.webp",
+      "/images/project/Doge/3_Result_Success.webp",
+      "/images/project/Doge/7_Inventory.png",
+    ],
+    type: "vertical",
+    source: "webp 포맷을 지원하는 브라우저로 열어주세요. 애플 OS에선 호환성이 낮습니다.<br/>영상이 로드되는데 시간이 걸릴 수 있어 기다려주시면 감사하겠습니다. 영상이 표시되지 않는다면 새로고침 해주세요.",
+
+    // 키워드~컬러 팔레트 없는 경우는 주석 처리
+    // keyword: ["키워드1", "키워드2", "키워드3"],
+    // keyShape: "/images/icon/unity.png",
+    // colorPalette: "/images/portfolio/projectS/colorPalette.png",
+    detailImage: [
+      {
+        // 1st
+        url: "/images/project/Doge/0_Tiltle.webp",
+        text: "타이틀 화면",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/1_Loading.webp",
+        text: "로딩 화면",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/2_Play.webp",
+        text: "플레이 화면",
+        type: "vertical",
+      },
+      {
+        // 2nd
+        url: "/images/project/Doge/3_Result_Success.webp",
+        text: "결과 화면 - 귀환",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/4_Result_Failed.webp",
+        text: "결과 화면 - 실패",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/5_Mailbox.png",
+        text: "우편함",
+        type: "vertical",
+      },
+      {
+        // 3rd
+        url: "/images/project/Doge/6_GetItem.webp",
+        text: "아이템 획득 연출",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/7_Inventory.png",
+        text: "인벤토리 화면",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/8_Equipment.png",
+        text: "장비 아이템 정보",
+        type: "vertical",
+      },
+      {
+        // 4th
+        url: "/images/project/Doge/9_Consumable.webp",
+        text: "소비 아이템 정보",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/10_ItemDiscard.png",
+        text: "아이템 버리기 팝업",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/11_Skill.png",
+        text: "스킬 화면",
+        type: "vertical",
+      },
+      {
+        // 5th
+        url: "/images/project/Doge/12_PlayerInfo.png",
+        text: "플레이어 정보",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/13_SocialPopup.webp",
+        text: "소셜 화면",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/14_FriendChat.webp",
+        text: "친구 채팅 화면",
+        type: "vertical",
+      },
+      {
+        // 6th
+        url: "/images/project/Doge/15_CashShop.png",
+        text: "캐시 상점",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/16_Shop_ItemTrade.png",
+        text: "아이템 거래 팝업",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/17_NoticePopup.png",
+        text: "공지사항",
+        type: "vertical",
+      },
+      {
+        // 7th
+        url: "/images/project/Doge/18_TermsPopup.png",
+        text: "약관 동의 팝업",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/19_BasicModal.png",
+        text: "기본 팝업",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/20_PartyInvite.webp",
+        text: "파티 초대 팝업",
+        type: "vertical",
+      },
+      {
+        // 8th
+        url: "/images/project/Doge/21_Snackbar.webp",
+        text: "초대 알림 스낵바",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/22_SettingPopup.png",
+        text: "설정 팝업",
+        type: "vertical",
+      },
+      {
+        url: "/images/project/Doge/23_ChannelSelect.png",
+        text: "채널 선택 팝업",
+        type: "vertical",
+      },
+      {
+        // 9th
+        url: "/images/project/Doge/24_PixelShader.webp",
+        text: "LED 셰이더",
+        type: "horizontal",
+      },
+      {
+        url: "/images/project/Doge/25_AlphaGradientShader.webp",
+        text: "알파 그라데이션 셰이더",
+        type: "horizontal",
+      },
+      {
+        // 10th
+        url: "/images/project/Doge/26_AdditionalBlending.webp",
+        text: "고급 블렌딩 모드 셰이더",
+        type: "horizontal",
+      },
+    ],
+    history: [
+      {
+        name: "프로젝트에서 작업한 내용",
+        list: [
+          {
+            title: "UI·UX",
+            content: [
+              { text: "UI·UX 명세 정리 및 관리", type: "text" },
+              { text: "UX 텍스트 및 번역 관리", type: "text" },
+              { text: "GUI, 연출 및 엔진 작업", type: "text" },
+            ],
+          },
+          {
+            title: "아트",
+            content: [
+              { text: "캐릭터 메카 장비 리소스 작업", type: "text" },
+              { text: "숲 배경 리소스 작업", type: "text" },
+              { text: "배경 아트 리소스 외주 발주 및 관리", type: "text" },
+            ],
+          },
+          {
+            title: "셰이더 작성",
+            content: [
+              { text: "UI 셰이더 작업", type: "text" },
+              {
+                // 들여쓰기 텍스트
+                text: "추가 블렌딩 모드 셰이더",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "LED 셰이더",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "알파 그라데이션 셰이더",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "SDF 셰이더",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "월드맵 셰이더 (클라이언트 개발자와 협업)",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "재련 레벨 셰이더 (클라이언트 개발자와 협업)",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "Text Mesh Pro 셰이더 및 스크립트 - 추가 Underlay 옵션",
+                content: ["text", "text"],
+                type: "list",
+              },
+              { text: "캐릭터 셰이더 (클라이언트 개발자와 협업)", type: "text" },
+              { text: "VFX 바리에이션 셰이더 - 파티클 시스템의 CustomData를 통해서 이펙트 텍스처의 색 조정", type: "text" },
+              // 들여쓰기 두번 텍스트 작성 예
+              // {
+              //   text: "TMP 셰이더 작업",
+              //   content: ["text", "text"],
+              //   type: "list",
+              // },
+            ],
+          },
+          {
+            title: "C# 스크립트 작성",
+            content: [
+              { text: "아트 리소스 작업 효율화", type: "text" },
+              {
+                // 들여쓰기 텍스트
+                text: "Order In Layer 변경 스크립트",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "애니메이션 키 일괄 수정 스크립트",
+                content: ["text", "text"],
+                type: "list",
+              },
+              {
+                // 들여쓰기 텍스트
+                text: "VFX 테스트 환경 작업 - 서버 연결 없이 전체적인 이펙트 동작을 확인할 수 있도록",
+                content: ["text", "text"],
+                type: "list",
+              },
+              { text: "UI 연출용 스크립트", type: "text" },
+              {
+                // 들여쓰기 텍스트
+                text: "텍스트 출력 연출 스크립트",
+                content: ["text", "text"],
+                type: "list",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "프로젝트를 통해 얻은 경험",
+        list: [
+          {
+            title: "UI 부문 R&D 주도적 진행",
+            content: [
+              {
+                text: "피그마 프로그램 사용과 DoTween, Text Mesh Pro, 배경 블러 기능 추가 등의 UI 관련 연구개발을 주도적으로 진행했습니다.<br/><br/>R&D를 진행하면서 특히 클라이언트 개발자 분들과 커뮤니케이션하는 일이 많았고, 그렇게 협업을 진행하다 보니 기술 관련 커뮤케이션 능력이 많이 향상되었습니다. R&D를 진행한 부분들로 인해 여러가지 면에서 프로젝트의 퀄리티도 상승한 것 같습니다.",
+                type: "text",
+              },
+            ],
+          },
+          {
+            title: "유니티 픽셀 셰이더",
+            content: [
+              {
+                text: "장비 아이템의 색을 변경하는 게임 내 컨텐츠를 만들기 위해서 스프라이트 셰이더에 대한 학습을 시작했습니다. 이후 UI와 이펙트에서 사용하는 셰이더들의 R&D를 진행하면서 유니티 빌트인 렌더 파이프라인에서의 픽셀 셰이더에 대한 지식 수준을 늘릴 수 있었습니다.<br/><br/>TMP(Text Mesh Pro) 텍스트의 최적화도 진행했습니다. 기존에는 텍스트에 외곽선과 그림자 효과를 여러 개 적용해야 할 때 TMP 텍스트를 여러번 겹쳐 사용했었는데, TMP 텍스트 하나에 효과 여러가지를 적용할 수 있도록 개선했습니다.<br/><br/>그렇게 셰이더에 대한 이해도를 늘리면서 그래픽스 최적화에 대해서도 배울 수 있었고, 셰이더를 통해 기존에 사용하지 못했던 시각적 표현들을 사용할 수 있게 되면서 작업의 스펙트럼 또한 넓어진 것 같습니다.",
+                type: "text",
+              },
+            ],
+          },
+          {
+            title: "리소스 수정 작업 자동화",
+            content: [
+              {
+                text: "프로토타입 개발 단계에서 캐릭터들의 크기 등 아트 리소스들이 변경되는 경우가 잦았습니다. 그 과정에서 기존에 작업된 스프라이트의 Order In Layer 값이나 애니메이션 값들을 변경된 아트 리소스에 맞추어 수작업으로 일일히 수정하는 번거로운 일들이 생겼고, 이러한 작업에 들어가는 시간을 줄이기 위해서 Order 값, 애니메이션 키값을 일괄적으로 변경해주는 스크립트를 작성했습니다.<br/><br/>스크립트로 번거로운 작업들을 자동화하자, 같이 작업하시는 아트 작업자분께서도 좋은 반응을 보여주셨고, 추후에 수정 사항이 추가로 발생하는 경우에도 가볍게 대응이 가능했습니다.",
+                type: "text",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
     name: "Spooky Friends",
     genre: ["모바일", "ZEPETO", "수집형", "캐주얼"],
     date: "2023.",
